ee5ff32816e608c167fe6bd4e4da2176f14393ce
karol1990 <pawlowskikarol1990@gmail.com>
2016-04-14 23:37:22 +0200

    Basic container operations

---
+++ Yeti.Tests/Container_Tests.cs
@@ -0,0 +1,32 @@
+﻿using Shouldly;
+using Xunit;
+using Yeti;
+
+namespace Yeti.Tests
+{
+    public class Container_Tests
+    {
+        [Fact]
+        public void Resolves_Registered_Instance()
+        {
+            var container = new Container();
+            container.Register<ITest, Test>();
+
+            var resolved = container.Resolve<ITest>();
+            resolved.ShouldBeOfType<ITest>();
+        }
+    }
+
+    public interface ITest
+    {
+        void Call();
+    }
+
+    public class Test : ITest
+    {
+        public void Call()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Yeti/Container.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Yeti
+{
+    public class Container
+    {
+        readonly IDictionary<Type, Type> _registrations = new Dictionary<Type, Type>();
+        public void Register<TRegistration, TImplementation>()
+        {
+            _registrations.Add(typeof(TRegistration), typeof(TImplementation));
+        }
+
+        public T Resolve<T>()
+        {
+            Type requestedToResolve = typeof(T);
+            Type type = _registrations[requestedToResolve];
+
+            return (T)Activator.CreateInstance(type);
+        }
+    }
+}
